b19dda0db0a1efe0be4bab12aa4d31cfb79e8b6c
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-02-24 17:42:45 -0300

    build: add @fastify/cors
    npm install @fastify/cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import fastify, { FastifyInstance } from "fastify";
+import { cardRoutes } from "./routes/card.routes";
+
+import cors from "@fastify/cors";
 
 const PORT = 3100;
 const BASE_URL = "http://localhost";
 
 const app: FastifyInstance = fastify();
+
+const setCors = async () => {
+    await app.register(cors, {
+        origin: "*",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    });
+};
+
+setCors();
 
 app.listen(
     {
