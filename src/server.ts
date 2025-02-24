94fd73d26afa3fb3c2d5185b82c5e7923d9b7b4e
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-02-24 17:06:28 -0300

    feat: start server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("######## yugioh card persistence api  ########");
+import fastify, { FastifyInstance } from "fastify";
+
+const PORT = 3100;
+const BASE_URL = "http://localhost";
+
+const app: FastifyInstance = fastify();
+
+app.listen(
+    {
+        port: PORT,
+    },
+    () => {
+        console.log(`[ server ] running on ${BASE_URL}/${PORT}`);
+    }
+);
